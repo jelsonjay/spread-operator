cc91e92240068330e633189cd369456838f482ec
Jelson J <jelson20outlook.com>
2020-08-31 16:47:40 +0100

    Update app.js

---
+++ js/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+let fruts = ['Banana', 'Avacado', 'Apple', 'Blackberry', 'Guava', 'Mango'];
+let vegetables = [
+	'Broccoli',
+	'Bell pepper',
+	'Tomato',
+	'Cucumber',
+	'Mushroom',
+	'Carrot'
+];
+let foodList = ['Bread', ''];
